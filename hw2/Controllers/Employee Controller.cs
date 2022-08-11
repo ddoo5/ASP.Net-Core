26acb79503e79f873deadb0b75669852f16a73ec
D <kritantablake@gmail.com>
2022-08-11 22:00:38 +0300

    Controllers

---
+++ hw2/Controllers/Employee Controller.cs
@@ -0,0 +1,56 @@
+﻿using ContractControlCentre.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractControlCentre.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class Employee_Controller : ControllerBase
+    {
+        private readonly ILogger<Employee_Controller> _logger;
+
+
+        public Employee_Controller(ILogger<Employee_Controller> logger)
+        {
+            this._logger = logger;
+        }
+
+
+        [HttpGet("get")]
+        public IActionResult GetAll()
+        {
+            return Ok("ok");
+        }
+
+
+        [HttpGet("get/{id}")]
+        public IActionResult GetById([FromQuery] int id)
+        {
+            return Ok();
+        }
+
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] EmployeeRegisterRequest request)
+        {
+            return Ok();
+        }
+
+
+        [HttpPut("update/{id}")]
+        public IActionResult UpdateById([FromQuery] int id)
+        {
+            return Ok();
+        }
+
+
+        [HttpDelete("delete/{id}")]
+        public IActionResult DeleteById([FromQuery] int id)
+        {
+            return Ok();
+        }
+    }
+
+
+}
+
---
+++ hw2/Controllers/Invoice Controller.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContractControlCentre.Controllers
+{
+    [ApiController]
+    [Route("api/invoices")]
+    public class Invoice_Controller : ControllerBase
+    {
+
+        private readonly ILogger<Invoice_Controller> _logger;
+
+
+        public Invoice_Controller(ILogger<Invoice_Controller> logger)
+        {
+            this._logger = logger;
+        }
+
+
+        [HttpGet("get")]
+        public IActionResult GetAll()
+        {
+            return Ok("ok");
+        }
+
+
+        [HttpPut("update/{id}")]
+        public IActionResult UpdateById([FromQuery] int id)
+        {
+            return Ok();
+        }
+    }
+}
+
