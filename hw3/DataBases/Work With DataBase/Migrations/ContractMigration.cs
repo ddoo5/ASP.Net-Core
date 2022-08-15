5ee07daff68220984de3bbef598c2dce82104f1f
D <kritantablake@gmail.com>
2022-08-15 14:29:39 +0300

    migrations

---
+++ hw3/DataBases/Work With DataBase/Migrations/ContractMigration.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace ContractControlCentre.DB.Migrations
+{
+    [Migration(1)]
+    public class ContractMigration : Migration
+	{
+        public override void Down()
+        {
+            Delete.Table("Contract_DB");
+        }
+
+
+        public override void Up()
+        {
+            Create.Table("Contract_DB")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Value").AsInt32()
+                .WithColumn("Time").AsTime();
+        }
+    }
+}
+
---
+++ hw3/DataBases/Work With DataBase/Migrations/InvoiceMigration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FluentMigrator;
+
+namespace ContractControlCentre.DB.Migrations
+{
+    [Migration(4)]
+    public class InvoiceMigration : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("Invoice_DB");
+        }
+
+
+        public override void Up()
+        {
+            Create.Table("Invoice_DB")
+                .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+                .WithColumn("Value").AsInt32()
+                .WithColumn("Time").AsTime();
+        }
+    }
+}
+
