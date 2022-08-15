b6eac431ffb922c7f157c981571dfa152d9a5fb5
D <kritantablake@gmail.com>
2022-08-15 14:30:06 +0300

    irepositories

---
+++ hw3/DataBases/Work With DataBase/Core/IRepositoryPerson.cs
@@ -0,0 +1,11 @@
+﻿namespace ContractControlCentre.DB.Core
+{
+    public interface IRepositoryPerson<T> where T : class
+    {
+        IList<T> GetAll();
+        T GetById(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
