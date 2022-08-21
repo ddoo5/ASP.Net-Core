53aca2e020cf8374f6c824934b1f7e3c8ffa2f5a
D <kritantablake@gmail.com>
2022-08-21 13:40:36 +0300

    controllers

---
+++ hw4/Controller/Controllers/Employee Controller.cs
@@ -0,0 +1,89 @@
+﻿using ContractControlCentre.DB.Entities;
+using ContractControlCentre.Requests;
+using ContractControlCentre.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractControlCentre.Controllers
+{
+    [ApiController]
+    [Route("api/employees")]
+    public class Employee_Controller : ControllerBase     //todo add documentation on swagger
+    {
+        private readonly ILogger<Employee_Controller> _logger;
+        private readonly EmployeeService _service;
+
+
+
+        public Employee_Controller(ILogger<Employee_Controller> logger, EmployeeService service)
+        {
+            this._logger = logger;
+            this._service = service;
+        }
+
+
+        [HttpGet("getById")]
+        public IActionResult GetById([FromQuery] int id)
+        {
+            var employee = _service.GetById(id);
+
+            if (employee != null)
+            {
+                return Ok(employee);
+            }
+            else
+            {
+                return BadRequest($"Employee with Id:{id} not found");
+            }
+        }
+
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] EmployeeRegisterRequest request)
+        {
+            var message = _service.Create(request);
+
+            if (message[0] == 'E')   //bad check, i know
+            {
+                return Ok(message);
+            }
+            else
+            {
+                return BadRequest(message);
+            }
+        }
+
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] EmployeeModelEntity request)
+        {
+            var message = _service.Update(request);
+
+            if (message[0] == 'U')   //same bad check
+            {
+                return Ok($"Person with Id:{request.Id} updated");
+            }
+            else
+            {
+                return BadRequest(message);
+            }
+        }
+
+
+        [HttpDelete("deleteById")]
+        public IActionResult DeleteById([FromQuery] int id)
+        {
+            var message = _service.DeleteById(id);
+
+            if (message[0] == 'D')
+            {
+                return Ok($"Person with Id:{id} deleted");
+            }
+            else
+            {
+                return BadRequest(message);
+            }
+        }
+
+    }
+}
+
---
+++ hw4/Controller/Controllers/Person Controller.cs
@@ -0,0 +1,119 @@
+﻿ using ContractControlCentre.DB.Entities;
+using ContractControlCentre.Requests;
+using ContractControlCentre.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractControlCentre.Controllers
+{
+    [ApiController]
+    [Route("api/persons")]
+    public class Person_Controller : ControllerBase    //todo add documentation on swagger
+    {
+        private readonly ILogger<Person_Controller> _logger;
+        private readonly PersonService _service;
+
+
+
+        public Person_Controller(ILogger<Person_Controller> logger, PersonService service)
+        {
+            this._logger = logger;
+            this._service = service;
+        }
+
+
+
+        [HttpGet("getById")]
+        public IActionResult GetById([FromQuery] int id)
+        {
+            var person = _service.GetById(id);
+
+            if(person != null)
+            {
+                return Ok(person);
+            }
+            else
+            {
+                return BadRequest($"Person with Id:{id} not found");
+            }
+        }
+
+
+        [HttpGet("searchByName")]
+        public IActionResult GetByName([FromQuery] string searchTerm)
+        {
+            var table = _service.GetByName(searchTerm);
+
+            if(table != null)
+            {
+                return Ok(table);
+            }
+            else
+            {
+                return BadRequest($"Person(s) with name: {searchTerm} not found");
+            }
+        }
+
+
+        [HttpGet("searchByPagination")]
+        public IActionResult GetByPagination([FromQuery] int skip, [FromQuery] int take)
+        {
+            var table = _service.GetByPagination(skip, take);
+
+            if(table != null)
+            {
+                return Ok(table);
+            }
+            else
+            {
+                return BadRequest($"Norhing found on page {take}");
+            }
+        }
+
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] PersonRegisterRequest request)
+        {
+            var message = _service.Create(request);
+
+            if (message[0] == 'P')   //bad check, i know
+            {
+                return Ok(message);
+            }
+            else
+            {
+                return BadRequest(message);
+            }
+        }
+
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] PersonModelEntity request)
+        {
+            var message = _service.Update(request);
+            if (message[0] == 'U')   //same bad check
+            {
+                return Ok($"Person with Id:{request.Id} updated");
+            }
+            else
+            {
+                return BadRequest(message);
+            }
+        }
+
+
+        [HttpDelete("deleteById")]
+        public IActionResult DeleteById([FromQuery] int id)
+        {
+            var message = _service.DeleteById(id);
+            if (message[0] == 'D')
+            {
+                return Ok($"Person with Id:{id} deleted");
+            }
+            else
+            {
+                return BadRequest(message);
+            }
+        }
+    }
+}
+
