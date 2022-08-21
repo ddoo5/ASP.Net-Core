1db9584078053b09cfdb85f2d6d14ae48342b10c
D <kritantablake@gmail.com>
2022-08-21 13:41:10 +0300

    irepositories

---
+++ hw4/DataBases/Work With DataBase/Core/IRepositoryPerson.cs
@@ -0,0 +1,14 @@
+﻿using ContractControlCentre.DB.Entities;
+
+namespace ContractControlCentre.DB.Core
+{
+    public interface IRepositoryPerson<T> where T : class
+    {
+        T GetById(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        IList<T> GetByName(string item);
+        IList<T> GetByPagination(int skip, int take);
+    }
+}
