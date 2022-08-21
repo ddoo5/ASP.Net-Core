1385f6affa3593b04a6d5d75e033d568d75e0672
D <kritantablake@gmail.com>
2022-08-21 13:41:37 +0300

    model entities

---
+++ hw4/DataBases/Work With DataBase/Data base connection/Entity/EmployeeModelEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContractControlCentre.DB.Entities
+{
+    public class EmployeeEntity<TUniqueId>
+    {
+        [Key]
+        [Column("Id")]
+        public TUniqueId Id { get; set; }
+
+        [Column("IsDeleted")]
+        public bool IsDeleted { get; set; }
+    }
+
+
+    [Table("Employee", Schema = "Work")]
+    public sealed class EmployeeModelEntity : EmployeeEntity<int>
+    {
+        [Column("First Name")]
+        public string FirstName { get; set; }
+
+        [Column("Last Name")]
+        public string LastName { get; set; }
+
+        [Column("Email")]
+        public string Email { get; set; }
+
+        [Column("Company")]
+        public string Company { get; set; }
+
+        [Column("Age")]
+        public int Age { get; set; }
+    }
+}
+
---
+++ hw4/DataBases/Work With DataBase/Data base connection/Entity/PersonModelEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContractControlCentre.DB.Entities
+{
+    public class PersonEntity<TUniqueId>
+    {
+        [Key]
+        [Column("Id")]
+        public TUniqueId Id { get; set; }
+
+        [Column("IsDeleted")]
+        public bool IsDeleted { get; set; }
+    }
+
+    [Table("Persons", Schema = "Work")]
+    public sealed class PersonModelEntity : PersonEntity<int>
+    {
+        [Column("First Name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Column("Last Name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Column("Email")]
+        public string Email { get; set; } = string.Empty;
+
+        [Column("Company")]
+        public string Company { get; set; } = string.Empty;
+
+        [Column("Age")]
+        public int Age { get; set; } = 0;
+    }
+}
+
