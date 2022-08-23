bfa722920f921d7028bcc3c35e080636429f22b9
D <kritantablake@gmail.com>
2022-08-23 19:35:25 +0300

    add auth methods to person and employee services

---
+++ hw5/Service/EmployeeService.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using ContractControlCentre.DB.Entities;
+using ContractControlCentre.DB.Interfaces;
+using ContractControlCentre.Requests;
+using ContractControlCentre.Security.Authentication.Service;
+using ContractControlCentre.Security.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractControlCentre.Service
+{
+    public class EmployeeService
+    {
+
+        private readonly ILogger<EmployeeService> _logger;
+        private readonly IEmployeeRepository _employeeRepo;
+        private readonly IAuthenticationService _authenticationService;
+
+
+
+        public EmployeeService(ILogger<EmployeeService> logger, IEmployeeRepository employeeRepo, IAuthenticationService authenticationService)
+        {
+            this._logger = logger;
+            this._employeeRepo = employeeRepo;
+            this._authenticationService = authenticationService;
+        }
+
+
+
+        /// <summary>
+        /// <para>Searching entity by Id</para>
+        /// </summary>
+        /// <param name="id">Id of entity wich need to search</param>
+        /// <returns>found entity or message with exception</returns>
+        public EmployeeModelEntity GetById(int id)
+        {
+            _logger.LogInformation(1, "Method 'GetById' in 'EmployeeService' launched");
+
+            try
+            {
+                var employee = _employeeRepo.GetById(id);
+
+                if (employee != null)
+                {
+                    _logger.LogInformation(1, "Method 'GetById' in 'EmployeeService' ended successfully");
+                    return employee;
+                }
+                else
+                {
+                    _logger.LogInformation(2, "Method 'GetById' in 'EmployeeService' ended successfully with nothing found");
+
+                    return null;
+                }
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'GetById' in 'EmployeeService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// <para>Creating entity</para>
+        /// </summary>
+        /// <param name="request">request from API, which include information about new entity</param>
+        /// <returns>Create entity or returns message with exception</returns>
+        public string Create(EmployeeRegisterRequest request)
+        {
+            _logger.LogInformation(1, "Method 'Create' in 'EmployeeService' launched");
+
+            try
+            {
+                _employeeRepo.Create(new EmployeeModelEntity
+                {
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    Email = request.Email,
+                    Company = request.Company,
+                    Age = request.Age,
+                    IsDeleted = false
+                });
+                _logger.LogInformation(1, "Method 'Create' in 'EmployeeService' ended successfully");
+
+                return $"Employee with name {request.FirstName} {request.LastName} registered";
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'Create' in 'EmployeeService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// <para>Updating entity</para>
+        /// </summary>
+        /// <param name="request">request from API, which include information about entity</param>
+        /// <returns>Update entity or returns message with exception</returns>
+        public string Update(EmployeeModelEntity request)
+        {
+            _logger.LogInformation(1, "Method 'Update' in 'EmployeeService' launched");
+
+            try
+            {
+                _employeeRepo.Update(request);
+
+                _logger.LogInformation(1, "Method 'Update' in 'EmployeeService' ended successfully");
+
+                return "Updated";
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'Update' in 'EmployeeService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// <para>Deleting entity by id</para>
+        /// </summary>
+        /// <param name="id">id of entity which should be deleted</param>
+        /// <returns>Delete entity or returns message with exception</returns>
+        public string DeleteById(int id)
+        {
+            _logger.LogInformation(1, "Method 'DeleteById' in 'EmployeeService' launched");
+
+            try
+            {
+                var employee = _employeeRepo.GetById(id);
+
+                if (employee != null)
+                {
+                    _employeeRepo.Delete(id);
+
+                    _logger.LogInformation(1, "Method 'DeleteById' in 'EmployeeService' ended successfully");
+
+                    return $"Deleted employee with id: {employee.Id}";
+                }
+                else
+                {
+                    _logger.LogInformation(2, "Method 'DeleteById' in 'EmployeeService' ended successfully with nothing found");
+
+                    return null;
+                }
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'DeleteById' in 'EmployeeService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Creating token for authenticate 
+        /// </summary>
+        /// <param name="username">User name(like id)
+        /// <remarks>,for each controller saved each sudo-user</remarks>
+        /// </param>
+        /// <param name="password">Password for authenticate</param>
+        /// <returns>Token or null</returns>
+        public TokenResponse Authenticate([FromQuery] string username, string password)
+        {
+            _logger.LogInformation(1, "Method 'Authenticate' in 'EmployeeService' launched");
+
+            try
+            {
+                if (username != "employee")
+                {
+                    _logger.LogInformation(2, "Method 'Authenticate' in 'EmployeeService' ended successfully with nothing found");
+                    return null;
+                }
+                else if (username == "employee")
+                {
+                    TokenResponse token = _authenticationService.Authentication(username, password);
+
+                    _logger.LogInformation(1, "Method 'Authenticate' in 'EmployeeService' ended successfully");
+
+                    return token;
+                }
+
+                _logger.LogInformation(2, "Method 'Authenticate' in 'EmployeeService' ended successfully with nothing found");
+
+                return null;
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'Authenticate' in 'EmployeeService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+    }
+}
+
---
+++ hw5/Service/PersonService.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Xml.Linq;
+using ContractControlCentre.DB.Core;
+using ContractControlCentre.DB.Entities;
+using ContractControlCentre.DB.Interfaces;
+using ContractControlCentre.Requests;
+using ContractControlCentre.Security.Authentication.Service;
+using ContractControlCentre.Security.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContractControlCentre.Service
+{
+    public class PersonService
+    {
+
+        private readonly ILogger<PersonService> _logger;
+        private readonly IPersonRepository _personRepo;
+        private readonly IAuthenticationService _authenticationService;
+
+
+
+        public PersonService(ILogger<PersonService> logger, IPersonRepository personRepo, IAuthenticationService authenticationService)
+        {
+            this._logger = logger;
+            this._personRepo = personRepo;
+            this._authenticationService = authenticationService;
+        }
+
+
+
+        /// <summary>
+        /// <para>Searching entity by Id</para>
+        /// </summary>
+        /// <param name="id">Id of entity wich need to search</param>
+        /// <returns>found entity or message with exception</returns>
+        public PersonModelEntity GetById(int id)
+        {
+            _logger.LogInformation(1, "Method 'GetById' in 'PersonService' launched");
+
+            try
+            {
+                var person = _personRepo.GetById(id);
+
+                if (person != null)
+                {
+                    _logger.LogInformation(1, "Method 'GetById' in 'PersonService' ended successfully");
+                    return person;
+                }
+                else
+                {
+                    _logger.LogInformation(2, "Method 'GetById' in 'PersonService' ended successfully with nothing found");
+
+                    return null;
+                }
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'GetById' in 'PersonService' ended with excpetion: {a}");
+
+                return null ;
+            }
+        }
+
+
+        /// <summary>
+        /// <para>Creating entity</para>
+        /// </summary>
+        /// <param name="request">request from API, which include information about new entity</param>
+        /// <returns>Create entity or returns message with exception</returns>
+        public string Create(PersonRegisterRequest request)
+        {
+            _logger.LogInformation(1, "Method 'Create' in 'PersonService' launched");
+
+            try
+            {
+                _personRepo.Create(new PersonModelEntity
+                {
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    Email = request.Email,
+                    Company = request.Company,
+                    Age = request.Age,
+                    IsDeleted = false
+                });
+                _logger.LogInformation(1, "Method 'Create' in 'PersonService' ended successfully");
+
+                return $"Person with name {request.FirstName} {request.LastName} registered";
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'Create' in 'PersonService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// <para>Updating entity</para>
+        /// </summary>
+        /// <param name="request">request from API, which include information about entity</param>
+        /// <returns>Update entity or returns message with exception</returns>
+        public string Update(PersonModelEntity request)
+        {
+            _logger.LogInformation(1, "Method 'Update' in 'PersonService' launched");
+
+            try
+            {
+                _personRepo.Update(request);
+
+                _logger.LogInformation(1, "Method 'Update' in 'PersonService' ended successfully");
+
+                return "Updated";
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'Update' in 'PersonService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// <para>Deleting entity by id</para>
+        /// </summary>
+        /// <param name="id">id of entity which should be deleted</param>
+        /// <returns>Delete entity or returns message with exception</returns>
+        public string DeleteById(int id)
+        {
+            _logger.LogInformation(1, "Method 'DeleteById' in 'PersonService' launched");
+
+            try
+            {
+                var person = _personRepo.GetById(id);
+
+                if(person != null)
+                {
+                    _personRepo.Delete(id);
+
+                    _logger.LogInformation(1, "Method 'DeleteById' in 'PersonService' ended successfully");
+
+                    return $"Deleted person with id: {person.Id}";
+                }
+                else
+                {
+                    _logger.LogInformation(2, "Method 'DeleteById' in 'PersonService' ended successfully with nothing found");
+
+                    return null;
+                }
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'DeleteById' in 'PersonService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// <para>Searching entit(y)/(ies) by name</para>
+        /// </summary>
+        /// <param name="name">name of entity which will be found</param>
+        /// <returns>Entities or returns message with exception</returns>
+        public IList<PersonModelEntity> GetByName(string name)
+        {
+            _logger.LogInformation(1, "Method 'GetByName' in 'PersonService' launched");
+
+            try
+            {
+                var table = _personRepo.GetByName(name);
+
+                if(table != null)
+                {
+                    _logger.LogInformation(1, "Method 'GetByName' in 'PersonService' ended successfully");
+
+                    return table;
+                }
+                else
+                {
+                    _logger.LogInformation(2, "Method 'GetByName' in 'PersonService' ended successfully with nothing found");
+
+                    return null;
+                }
+            }
+            catch(Exception a)
+            {
+                _logger.LogError(3, $"Method 'GetByName' in 'PersonService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Searching entit(y)/(ies) by pages
+        /// </summary>
+        /// <param name="skip">id which should be skipped</param>
+        /// <param name="take">id from will be searching</param>
+        /// <returns>Entities or returns message with exception</returns>
+        public IList<PersonModelEntity> GetByPagination(int skip, int take)
+        {
+            _logger.LogInformation(1, "Method 'GetByPagination' in 'PersonService' launched");
+
+            try
+            {
+                var table = _personRepo.GetByPagination(skip, take);
+
+                if(table != null)
+                {
+                    _logger.LogInformation(1, "Method 'GetByPagination' in 'PersonService' ended successfully");
+
+                    return table;
+                }
+                else
+                {
+                    _logger.LogInformation(2, "Method 'GetByPagination' in 'PersonService' ended successfully with nothing found");
+                    return null;
+                }
+            }
+            catch(Exception a)
+            {
+                _logger.LogError(3, $"Method 'GetByPagination' in 'PersonService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+
+        /// <summary>
+        /// Creating token for authenticate 
+        /// </summary>
+        /// <param name="username">User name(like id)
+        /// <remarks>,for each controller saved each sudo-user</remarks>
+        /// </param>
+        /// <param name="password">Password for authenticate</param>
+        /// <returns>Token or null</returns>
+        public TokenResponse Authenticate([FromQuery] string username, string password)
+        {
+            _logger.LogInformation(1, "Method 'Authenticate' in 'PersonService' launched");
+
+            try
+            {
+                if (username != "person")
+                {
+                    _logger.LogInformation(2, "Method 'Authenticate' in 'PersonService' ended successfully with nothing found");
+                    return null;
+                }
+                else if (username == "person")
+                {
+                    TokenResponse token = _authenticationService.Authentication(username, password);
+
+                    _logger.LogInformation(1, "Method 'Authenticate' in 'PersonService' ended successfully");
+
+                    return token;
+                }
+
+                _logger.LogInformation(2, "Method 'Authenticate' in 'PersonService' ended successfully with nothing found");
+                return null;
+            }
+            catch (Exception a)
+            {
+                _logger.LogError(3, $"Method 'Authenticate' in 'PersonService' ended with excpetion: {a}");
+
+                return null;
+            }
+        }
+
+    }
+}
+
