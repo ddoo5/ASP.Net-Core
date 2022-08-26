3793eb1fcddb213fe97ee0cb81d5e43fb4f818db
D <kritantablake@gmail.com>
2022-08-26 19:17:28 +0300

    fluent validation service

---
+++ hw6/Validation/messages/FluentValidationService.cs
@@ -0,0 +1,31 @@
+﻿using ContractControlCentre.Validation.Models;
+using ContractControlCentre.Validation.Models.Interfaces;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace ContractControlCentre.Validation.Messages
+{
+    public abstract class FluentValidationService<TEntity> : AbstractValidator<TEntity>, IValidationService<TEntity>
+    where TEntity : class
+    {
+        public IReadOnlyList<IOperationFailure> ValidateEntity(TEntity item)
+        {
+            ValidationResult result = Validate(item);
+
+            if (result is null || result.Errors.Count == 0)
+            {
+                return ArraySegment<IOperationFailure>.Empty;
+            }
+
+            List<IOperationFailure> failures = new List<IOperationFailure>(result.Errors.Count);
+
+            foreach (ValidationFailure error in result.Errors)
+            {
+                OperationFailure failure = new OperationFailure(); failure.PropertyName = error.PropertyName; failure.Description = error.ErrorMessage; failure.Code = error.ErrorCode;
+                failures.Add(failure);
+            }
+            return failures;
+        }
+    }
+}
+
