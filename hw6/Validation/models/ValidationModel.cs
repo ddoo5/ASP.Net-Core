cf0d41f204595b2c65a053fa88087ae317d40807
D <kritantablake@gmail.com>
2022-08-26 19:17:54 +0300

    validation models

---
+++ hw6/Validation/models/ValidationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ContractControlCentre.Validation.Models
+{
+    public sealed class ValidationModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+    }
+}
+
