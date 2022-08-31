e0d09ed2a94a755030e89a3b43ae08bba0ab0d0f
D <kritantablake@gmail.com>
2022-08-31 23:44:10 +0300

    tests

---
+++ hw7/FactTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using ContractControlCentre.Controllers;
+using ContractControlCentre.DB.Entities;
+using ContractControlCentre.DB.Interfaces;
+using ContractControlCentre.Requests;
+using ContractControlCentre.Security.Authentication.Service;
+using ContractControlCentre.Service;
+using ContractControlCentre.Validation.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace CCC.Tests
+{
+    public class FactTests
+    {
+        [Fact]
+        public void PersonConnectionWithDB_Create_Test()
+        {
+            var fix = new Fixture().Customize(new AutoMoqCustomization());
+            PersonService _service = fix.Create<PersonService>();
+
+            PersonRegisterRequest person1 = new PersonRegisterRequest()
+            {
+                Age = 0,
+                Company = "",
+                Email = "some",
+                FirstName = "i don't know",
+                LastName = "what is that?"
+            };
+
+            PersonRegisterRequest person2 = new PersonRegisterRequest()
+            {
+                Age = 23,
+                Company = "LLC",
+                Email = "testwedcs@gmail.com",
+                FirstName = "Jhohn",
+                LastName = "Robert"
+            };
+
+            Regex expectedMessage = new($"Person with name {person1.FirstName} {person1.LastName} registered");
+            Regex expectedMessage2 = new($"Person with name {person2.FirstName} {person2.LastName} registered");
+
+            //Act
+            string result = _service.Create(person1);
+            string result2 = _service.Create(person2);
+
+            //Assert
+            Assert.DoesNotMatch(expectedMessage, result);
+            Assert.Matches(expectedMessage2, result2);
+        }
+
+
+        [Fact]
+        public void PersonConnectionWithDB_Update_Test()
+        {
+            // Arrange
+            var fix = new Fixture().Customize(new AutoMoqCustomization());
+            PersonService _service = fix.Create<PersonService>();
+
+            PersonModelEntity person1 = new PersonModelEntity()
+            {
+                Age = 0,
+                Company = "",
+                Email = "some",
+                FirstName = "i don't know",
+                LastName = "what is that?",
+                Id = -4,
+                IsDeleted = true
+            };
+
+            PersonModelEntity person2 = new PersonModelEntity()
+            {
+                Age = 23,
+                Company = "LLc",
+                Email = "test@gmail.com",
+                FirstName = "Jhon",
+                LastName = "Roberto",
+                Id=3,
+                IsDeleted=true
+            };
+
+            Regex expectedMessage = new("Updated");
+            Regex expectedMessage2 = new("Updated");
+
+            //Act
+            string result = _service.Update(person1);
+            string result2 = _service.Update(person2);
+
+            //Assert
+            Assert.Matches(expectedMessage, result);
+            Assert.Matches(expectedMessage2, result2);
+        }
+    }
+}
+
---
+++ hw7/TheoryTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Castle.Core.Logging;
+using ContractControlCentre.DB.Connections;
+using ContractControlCentre.DB.Entities;
+using ContractControlCentre.DB.Interfaces;
+using ContractControlCentre.DB.Repositories;
+using ContractControlCentre.Requests;
+using ContractControlCentre.Security.Authentication.Service;
+using ContractControlCentre.Service;
+using ContractControlCentre.Validation.Models.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CCC.Tests;
+
+public class TheoryTests
+{
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(1000)]
+    public void PersonService_GetById_Test(int id)
+    {
+        // Arrange
+        PersonRepoForDB _repository = new(new DbConnection());
+
+        var person = new PersonModelEntity() {
+            Age = 0,
+            Company = string.Empty,
+            Email = string.Empty,
+            FirstName = string.Empty,
+            LastName = string.Empty};
+
+        //Act
+        var a = _repository.GetById(id);
+
+        //Assert
+        if(a.Age >= 0)
+            Assert.NotNull(a);
+        else
+            Assert.False(false);
+    }
+
+
+    [Theory]
+    [InlineData("Jhon")]
+    [InlineData("string")]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void PersonService_GetByName_Test(string name)
+    {
+        // Arrange
+        var fix = new Fixture().Customize(new AutoMoqCustomization());
+        PersonService _service = fix.Create<PersonService>();
+
+        //Act
+        var result = _service.GetByName(name);
+
+        //Assert
+        if (result.Count > 0)
+            Assert.NotEmpty(result);
+        else
+            Assert.Empty(result);
+    }
+
+
+    [Theory]
+    [InlineData(-1,10)]
+    [InlineData(0,20)]
+    [InlineData(20,13)]
+    [InlineData(2000, 13)]
+    public void PersonService_GetByPagination_Test(int skip, int take)
+    {
+        // Arrange
+        var fix = new Fixture().Customize(new AutoMoqCustomization());
+        PersonService _service = fix.Create<PersonService>();
+
+        //Act
+        var result = _service.GetByPagination(skip, take);
+
+        //Assert
+        if(result.Count > 0)
+            Assert.NotEmpty(result);
+        else
+            Assert.Empty(result);
+    }
+
+
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(10)]
+    [InlineData(1000)]
+    [DisplayName("Delete by id TEST")]
+    public void PersonService_DeleteById_Test(int id)
+    {
+        // Arrange
+        PersonRepoForDB _repository = new(new DbConnection());
+
+        //Act
+        _repository.Delete(id);
+        var result = _repository.GetById(id);
+
+        //Assert
+        if(result.IsDeleted == true)
+            Assert.NotNull(result);
+    }
+}
