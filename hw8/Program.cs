55ba053e51a33eccab86db6c253f4fe4065fb3be
D <kritantablake@gmail.com>
2022-09-03 23:51:24 +0300

    Program

---
+++ hw8/Program.cs
@@ -0,0 +1,18 @@
+﻿using RichModelRelease.Billl.IRepositories;
+using RichModelRelease.Billl.Models;
+using RichModelRelease.Billl.Repositories;
+
+
+
+//for testing
+
+Bill bill = new(1, 154);
+BillRepository _repo = new(bill);
+
+_repo.IncludeSheetsPlus(2599);
+Console.WriteLine($"{bill.BillNumber} {bill.Sum}");
+
+_repo.IncludeSheetsMinus(799);
+Console.WriteLine($"{bill.BillNumber} {bill.Sum}");
+
+Console.ReadKey();
